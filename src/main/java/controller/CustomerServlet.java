325a61bbcbc174153a1bf969d81ad2557960bdfa
DuyLP2604 <duylpdce201148@gmail.com>
2026-07-10 08:52:21 +0700

    Partical merge fix, need refactor

--- src/main/java/controller/CustomerServlet.java
+++ src/main/java/controller/CustomerServlet.java
@@ -22,32 +22,6 @@ import model.Customer;
 @WebServlet(name = "CustomerServlet", urlPatterns = {"/customer"})
 public class CustomerServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet CustomerServlet</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet CustomerServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -104,6 +78,7 @@ public class CustomerServlet extends HttpServlet {
         String passport = request.getParameter("passport");
         String nation = request.getParameter("nation");
         
+        // missing UserId
         Customer c = new Customer(id, name, phone, email, address, cccd, passport, nation);
         
         if (action.equalsIgnoreCase("add")){
--- src/main/java/dao/EmployeeDAO.java
+++ src/main/java/dao/EmployeeDAO.java
@@ -132,7 +132,4 @@ public class EmployeeDAO extends DBContext {
             ex.printStackTrace();
         }
     }
-    >>>>>>> CUS
-    -and
-    -EMP
 }
